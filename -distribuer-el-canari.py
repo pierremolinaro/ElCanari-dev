43db83f3f0eb3379035d38ffe955e814a2a39cc2
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2019-02-23 21:21:53 +0100

    Version 0.4.5

--- -distribuer-el-canari.py
+++ -distribuer-el-canari.py
@@ -8,7 +8,7 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
 #-------------------- Version ElCanari
-VERSION_CANARI = "0.4.4"
+VERSION_CANARI = "0.4.5"
 
 #——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————*
 #   FOR PRINTING IN COLOR                                                                                              *
