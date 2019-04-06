105938b25f208da519e3206e3f3fd9afc15a5572
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2019-04-06 17:15:00 +0200

    New Font document format

--- ElCanari/document-font/customStore-characters.swift
+++ ElCanari/document-font/customStore-characters.swift
@@ -16,64 +16,113 @@ private let STORE_KEY = "-characters-"
 
 func customStore_FontCharacter_characters (_ inCharacters : [FontCharacter], intoDictionary : NSMutableDictionary) {
   var s = ""
-  var firstChar = true
   for char in inCharacters {
-    if firstChar {
-      firstChar = false
-    }else{
-      s += "|"
-    }
+    s += "|"
     s += "\(char.codePoint):"
     s += "\(char.advance):"
     s += char.mWarnsWhenAdvanceIsZero ? "1:" : "0:"
-    s += char.mWarnsWhenNoSegment ? "1:" : "0:"
-    var firstSegment = true
+    s += char.mWarnsWhenNoSegment ? "1" : "0"
+    var x = Int.min
+    var y = Int.min
     for segment in char.segments_property.propval {
-      if firstSegment {
-        firstSegment = false
+      if (segment.x1 == x) && (segment.y1 == y) {
+        s += ">\(segment.x2) \(segment.y2)"
+      }else if (segment.x1 == segment.x2) && (segment.y1 == segment.y2) {
+        s += ",\(segment.x1) \(segment.y1)"
       }else{
-        s += ","
+        s += ",\(segment.x1) \(segment.y1)>\(segment.x2) \(segment.y2)"
       }
-      s += "\(segment.x1) \(segment.y1) \(segment.x2) \(segment.y2)"
+      x = segment.x2
+      y = segment.y2
     }
   }
-  // NSLog ("LENGTH: \(s.utf8.count)")
+//  NSLog ("LENGTH: \(s.utf8.count)")
   intoDictionary.setValue (s, forKey: STORE_KEY)
 }
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 func customRead_FontCharacter_characters (from inDictionary : NSDictionary, with inUndoManager : EBUndoManager?) -> [FontCharacter] {
-  let start = Date ()
+//  let start = Date ()
   var result = [FontCharacter] ()
   if let s = inDictionary [STORE_KEY] as? String {
-    for charDefinition in s.components (separatedBy: "|") {
+    // Swift.print (s)
+    let scanner = Scanner (string: s)
+    var ok = true
+    while ok && scanner.myTestString ("|") {
       let newCharacter = FontCharacter (inUndoManager)
       result.append (newCharacter)
-      let charComponents = charDefinition.components (separatedBy: ":")
-      newCharacter.codePoint = Int (charComponents [0])!
-      newCharacter.advance = Int (charComponents [1])!
-      newCharacter.mWarnsWhenAdvanceIsZero = Int (charComponents [2])! != 0
-      newCharacter.mWarnsWhenNoSegment = Int (charComponents [3])! != 0
-      if charComponents [4] != "" {
-        var segments = [SegmentForFontCharacter] ()
-        for segment in charComponents [4].components (separatedBy: ",") {
+      newCharacter.codePoint = scanner.myScanInt (&ok)
+      scanner.myCheckString (":", &ok)
+      newCharacter.advance = scanner.myScanInt (&ok)
+      scanner.myCheckString (":", &ok)
+      newCharacter.mWarnsWhenAdvanceIsZero = scanner.myScanInt (&ok) != 0
+      scanner.myCheckString (":", &ok)
+      newCharacter.mWarnsWhenNoSegment = scanner.myScanInt (&ok) != 0
+    //--- Segments
+      var segments = [SegmentForFontCharacter] ()
+      while ok && scanner.myTestString (",") {
+        var x = scanner.myScanInt (&ok)
+        var y = scanner.myScanInt (&ok)
+        var singlePoint = true
+        while scanner.myTestString (">") {
+          singlePoint = false
           let newSegment = SegmentForFontCharacter (inUndoManager)
           segments.append (newSegment)
-          let xy = segment.components (separatedBy: " ")
-          newSegment.x1 = Int (xy [0])!
-          newSegment.y1 = Int (xy [1])!
-          newSegment.x2 = Int (xy [2])!
-          newSegment.y2 = Int (xy [3])!
+          newSegment.x1 = x
+          newSegment.y1 = y
+          x = scanner.myScanInt (&ok)
+          y = scanner.myScanInt (&ok)
+          newSegment.x2 = x
+          newSegment.y2 = y
         }
-        newCharacter.segments_property.setProp (segments)
+        if singlePoint {
+          let newSegment = SegmentForFontCharacter (inUndoManager)
+          segments.append (newSegment)
+          newSegment.x1 = x
+          newSegment.y1 = y
+          newSegment.x2 = x
+          newSegment.y2 = y
+        }
+        // Swift.print ("Segment \(newSegment.x1) \(newSegment.y1) \(newSegment.x2) \(newSegment.y2) > \(ok)")
       }
+      newCharacter.segments_property.setProp (segments)
     }
+//    NSLog ("ok \(ok)")
   }
-  let duration = Date ().timeIntervalSince (start)
-  NSLog ("duration \(duration)")
+//  let duration = Date ().timeIntervalSince (start)
+//  NSLog ("duration \(duration)")
   return result
 }
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
+extension Scanner {
+
+  //··················································································································
+
+  func myScanInt (_ ioOk : inout Bool) -> Int {
+    var value = 0
+    let b = self.scanInt (&value)
+    ioOk = ioOk && b
+    return value
+  }
+
+  //··················································································································
+
+  func myCheckString (_ inString : String, _ ioOk : inout Bool) {
+    let b = self.scanString (inString, into: nil)
+    ioOk = ioOk && b
+  }
+
+  //··················································································································
+
+  func myTestString (_ inString : String) -> Bool {
+    return self.scanString (inString, into: nil)
+  }
+
+  //··················································································································
+
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
