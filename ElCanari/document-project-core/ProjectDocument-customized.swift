f68a1a4c2e28d6f35153937677ed6041c8724dcc
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2019-10-03 13:51:30 +0200

    Correction création projet vide (marqué modifié)

--- ElCanari/document-project-core/ProjectDocument-customized.swift
+++ ElCanari/document-project-core/ProjectDocument-customized.swift
@@ -34,6 +34,7 @@ let TRACK_INITIAL_SIZE_CANARI_UNIT = 500 * 2_286 // # 500 mils
   //--- Add default net class and first sheet
     let netClass = NetClassInProject (self.ebUndoManager)
     self.rootObject.mNetClasses.append (netClass)
+    self.rootObject.mDefaultNetClassName = self.rootObject.mNetClasses [0].mNetClassName
     let sheet = SheetInProject (self.ebUndoManager)
     self.rootObject.mSheets.append (sheet)
     self.rootObject.mSelectedSheet = sheet
