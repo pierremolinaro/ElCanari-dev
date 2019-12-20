50e43bfc416c0551313e0779295c7a034d34330d
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2019-12-20 13:34:53 +0100

    Added doc in PDF

---
+++ ElCanari/application/ApplicationDelegate-elcanari-documentation.swift
@@ -0,0 +1,37 @@
+//
+//  ApplicationDelegate-elcanari-documentation.swift
+//  ElCanari
+//
+//  Created by Pierre Molinaro on 20/12/2019.
+//
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+import Cocoa
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+fileprivate var gWindow : NSWindow? = nil
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+extension ApplicationDelegate {
+
+  //····················································································································
+  //   OPEN ELCANARI DOCUMENTATION
+  //····················································································································
+
+  @IBAction func openElCanariDocumentationAction (_ inSender : Any?) {
+    if gWindow == nil,
+       let docPath = Bundle.main.path (forResource: "ElCanari-user-manual", ofType: "pdf"),
+       let data = FileManager ().contents (atPath: docPath) {
+      gWindow = CanariPDFWindow (fileName: docPath.lastPathComponent, pdfData: data)
+    }
+    gWindow?.makeKeyAndOrderFront (nil)
+  }
+
+  //····················································································································
+
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
