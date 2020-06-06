7c2257ceeb02c6b319cb23c56fb602560c56c72e
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2020-06-06 16:43:18 +0200

    Remplacement Freerouter par Freerouting

--- ElCanari/document-project-action/action-ProjectDocument-launchFreeRouterAction.swift
+++ ElCanari/document-project-action/action-ProjectDocument-launchFreeRouterAction.swift
@@ -13,17 +13,17 @@ import Cocoa
 extension ProjectDocument {
   @objc func launchFreeRouterAction (_ sender : NSObject?) {
 //--- START OF USER ZONE 2
-        if let freeRouterApplication : String = Bundle.main.path (forResource: "freerouter-pm", ofType: "app") {
+        if let freeRouterApplication : String = Bundle.main.path (forResource: "Freerouting", ofType: "app") {
           let ok = NSWorkspace.shared.launchApplication (freeRouterApplication)
           if !ok {
             let alert = NSAlert ()
-            alert.messageText = "Cannot launch FreeRouter application"
+            alert.messageText = "Cannot launch FreeRouting application"
             alert.beginSheetModal (for: self.windowForSheet!) { (NSModalResponse) in }
           }
         }else{
             let alert = NSAlert ()
             alert.messageText = "Internal Error"
-            alert.informativeText = "FreeRouter application does not exist."
+            alert.informativeText = "FreeRouting application does not exist."
             alert.beginSheetModal (for: self.windowForSheet!) { (NSModalResponse) in }
         }
 //--- END OF USER ZONE 2
