cf038043ffa69a1734e30abc4e55f3bbc30c579e
pierremolinaro <pierre@pcmolinaro.name>
2021-11-25 15:46:27 +0100

    Début Font document Auto Layout

---
+++ ElCanari/document — font-auto-layout/document-AutoLayoutFont-sub-class.swift.swift
@@ -0,0 +1,28 @@
+//
+//  document-AutoLayoutFont-sub-class.swift.swift
+//  ElCanari-Debug-temporary
+//
+//  Created by Pierre Molinaro on 24/11/2021.
+//
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+import Cocoa
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+let PMFontVersion = "PMFontVersion"
+let PMFontComment = "PMFontComment"
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+// let packagePasteboardType = NSPasteboard.PasteboardType (rawValue: "name.pcmolinaro.pasteboard.package")
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+@objc(AutoLayoutFontDocumentSubClass) final class AutoLayoutFontDocumentSubClass : AutoLayoutFontDocument {
+
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
