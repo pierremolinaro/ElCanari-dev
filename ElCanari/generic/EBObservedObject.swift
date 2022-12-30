faa3d9224f53ceb4aa0fd56650eae57980339fb4
pierremolinaro <pierre@pcmolinaro.name>
2022-12-30 14:25:57 +0100

    Update EBObservedObject.swift

--- ElCanari/generic/EBObservedObject.swift
+++ ElCanari/generic/EBObservedObject.swift
@@ -10,30 +10,32 @@ class EBObservedObject : EBSwiftBaseObject {
 
   //····················································································································
 
-  private final var mObservers = EBWeakEventSet ()
+  private var mDictionary = [Int : EBWeakObserverSetElement] ()
 
   //····················································································································
 
   final func startsToBeObserved (by inObserver : EBObserverProtocol) {
-    self.mObservers.insert (observer: inObserver)
+    let address = inObserver.objectIndex
+    self.mDictionary [address] = EBWeakObserverSetElement (observer: inObserver)
     inObserver.observedObjectDidChange ()
   }
 
   //····················································································································
 
   final func stopsBeingObserved (by inObserver : EBObserverProtocol) {
-    self.mObservers.remove (observer: inObserver)
+    let address = inObserver.objectIndex
+    self.mDictionary [address] = nil
     inObserver.observedObjectDidChange ()
   }
 
   //····················································································································
 
   func observedObjectDidChange () {
-    for entry in self.mObservers.values () {
+    for (key, entry) in self.mDictionary {
       if let observer = entry.possibleObserver {
         observer.observedObjectDidChange ()
       }else{
-        self.mObservers.triggerPacking ()
+        self.mDictionary [key] = nil
       }
     }
   }
@@ -44,60 +46,60 @@ class EBObservedObject : EBSwiftBaseObject {
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-@MainActor struct EBWeakEventSet {
-
-  //····················································································································
-
-  private var mDictionary = [Int : EBWeakObserverSetElement] ()
-
-  //····················································································································
-
-  mutating func values () -> Dictionary <Int, EBWeakObserverSetElement>.Values {
-    self.pack ()
-    return self.mDictionary.values
-  }
-
-  //····················································································································
-
-  private var mPackingTriggered = false
-
-  //····················································································································
-
-  mutating func triggerPacking () {
-    self.mPackingTriggered = true
-  }
-
-  //····················································································································
-
-  private mutating func pack () {
-    if self.mPackingTriggered {
-      self.mPackingTriggered = false
-      for (key, entry) in self.mDictionary {
-        if entry.possibleObserver == nil {
-          self.mDictionary [key] = nil
-        }
-      }
-    }
-  }
-
-  //····················································································································
-
-  mutating func insert (observer inObserver : EBObserverProtocol) {
-    let address = inObserver.objectIndex
-    self.mDictionary [address] = EBWeakObserverSetElement (observer: inObserver)
-    inObserver.observedObjectDidChange ()
-  }
-
-  //····················································································································
-
-  mutating func remove (observer inObserver : EBObserverProtocol) {
-    let address = inObserver.objectIndex
-    self.mDictionary [address] = nil
-    inObserver.observedObjectDidChange ()
-  }
-
-  //····················································································································
-
-}
+//@MainActor struct EBWeakEventSet {
+//
+//  //····················································································································
+//
+//  private var mDictionary = [Int : EBWeakObserverSetElement] ()
+//
+//  //····················································································································
+//
+//  mutating func values () -> Dictionary <Int, EBWeakObserverSetElement>.Values {
+//    self.pack ()
+//    return self.mDictionary.values
+//  }
+//
+//  //····················································································································
+//
+//  private var mPackingTriggered = false
+//
+//  //····················································································································
+//
+//  mutating func triggerPacking () {
+//    self.mPackingTriggered = true
+//  }
+//
+//  //····················································································································
+//
+//  private mutating func pack () {
+//    if self.mPackingTriggered {
+//      self.mPackingTriggered = false
+//      for (key, entry) in self.mDictionary {
+//        if entry.possibleObserver == nil {
+//          self.mDictionary [key] = nil
+//        }
+//      }
+//    }
+//  }
+//
+//  //····················································································································
+//
+//  mutating func insert (observer inObserver : EBObserverProtocol) {
+//    let address = inObserver.objectIndex
+//    self.mDictionary [address] = EBWeakObserverSetElement (observer: inObserver)
+//    inObserver.observedObjectDidChange ()
+//  }
+//
+//  //····················································································································
+//
+//  mutating func remove (observer inObserver : EBObserverProtocol) {
+//    let address = inObserver.objectIndex
+//    self.mDictionary [address] = nil
+//    inObserver.observedObjectDidChange ()
+//  }
+//
+//  //····················································································································
+//
+//}
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
