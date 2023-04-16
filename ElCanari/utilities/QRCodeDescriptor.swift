598000f800e07fcd87cfb11c5a2dfff729a5285c
pierremolinaro <pierre@pcmolinaro.name>
2023-04-16 15:07:08 +0200

    Amélioration génération QR Code

--- ElCanari/utilities/QRCodeDescriptor.swift
+++ ElCanari/utilities/QRCodeDescriptor.swift
@@ -5,6 +5,9 @@
 //  Created by Pierre Molinaro on 15/04/2023.
 //
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// https://www.keyence.com/ss/products/auto_id/codereader/basic_2d/qr.jsp
+// https://developer.apple.com/library/archive/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/filter/ci/CIQRCodeGenerator
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 import AppKit
 
@@ -46,62 +49,37 @@ struct QRCodeDescriptor : Hashable {
     ]
     let barcodeCreationFilter = CIFilter (name: "CIQRCodeGenerator", parameters: inputParams)!
     let ciImage = barcodeCreationFilter.outputImage!
-    let ciImageRepresentation = NSCIImageRep (ciImage: ciImage)
   //--- Build bit map
-    self.imageWidth = ciImageRepresentation.pixelsWide + (inFramed ? 2 : 0)
-    imageHeight = ciImageRepresentation.pixelsHigh + (inFramed ? 2 : 0)
-    let possibleOffscreenRep = NSBitmapImageRep (
-      bitmapDataPlanes: nil,
-      pixelsWide: ciImageRepresentation.pixelsWide,
-      pixelsHigh: ciImageRepresentation.pixelsHigh,
-      bitsPerSample: 8,
-      samplesPerPixel: 4,
-      hasAlpha: true,
-      isPlanar: false,
-      colorSpaceName: NSColorSpaceName.deviceRGB,
-      bitmapFormat: NSBitmapImageRep.Format.alphaFirst,
-      bytesPerRow: 0,
-      bitsPerPixel: 0
-    )
+    let bitMapImageRep = NSBitmapImageRep (ciImage: ciImage)
+    self.imageWidth = bitMapImageRep.pixelsWide + (inFramed ? 2 : 0)
+    self.imageHeight = bitMapImageRep.pixelsHigh + (inFramed ? 2 : 0)
+  //--- Build QR Code representation
     var rects = [QRCodeRectangle] ()
-    if let offscreenRep = possibleOffscreenRep,
-       let graphicContext = NSGraphicsContext (bitmapImageRep: offscreenRep) {
-      NSGraphicsContext.saveGraphicsState ()
-      NSGraphicsContext.current = graphicContext
-      graphicContext.imageInterpolation = .none
-      ciImageRepresentation.draw (in: NSRect (origin: .zero, size: ciImageRepresentation.size))
-      for y in 0 ..< ciImageRepresentation.pixelsHigh {
-        let rectOriginY = ciImageRepresentation.pixelsHigh - (inFramed ? 0 : 1) - y
-        var originX = 0
-        var width = 0 // Empty rect
-        for x in 0 ..< ciImageRepresentation.pixelsWide {
-          if let color = offscreenRep.colorAt (x: x, y: y) {
-            var redComponent : CGFloat = 0.0
-            var greenComponent : CGFloat = 0.0
-            var blueComponent : CGFloat = 0.0
-            var alphaComponent : CGFloat = 0.0
-            color.getRed (&redComponent, green:&greenComponent, blue:&blueComponent, alpha:&alphaComponent)
-            let blackPixel = redComponent < 0.5
-            if blackPixel {
-              if width == 0 { // Begin a new rect
-                originX = x + (inFramed ? 1 : 0)
-                width = 1
-              }else{ // Extend an existing rect
-                width += 1
-              }
-            }else if width > 0 { // White pixel, closing an existing rect
-              let r = QRCodeRectangle (x: originX, y: rectOriginY, width: width, height: 1)
-              rects.append (r)
-              width = 0
-            }
+    for y in 0 ..< bitMapImageRep.pixelsHigh {
+      let rectOriginY = bitMapImageRep.pixelsHigh - (inFramed ? 0 : 1) - y
+      var originX = 0
+      var width = 0 // Empty rect
+      for x in 0 ..< bitMapImageRep.pixelsWide {
+        var p : Int = 0
+        bitMapImageRep.getPixel (&p, atX: x, y: y)
+        let blackPixel = p < 128
+        if blackPixel {
+          if width == 0 { // Begin a new rect
+            originX = x + (inFramed ? 1 : 0)
+            width = 1
+          }else{ // Extend an existing rect
+            width += 1
           }
-        }
-        if width > 0 { // closing the last existing rect
+        }else if width > 0 { // White pixel, closing an existing rect
           let r = QRCodeRectangle (x: originX, y: rectOriginY, width: width, height: 1)
           rects.append (r)
+          width = 0
         }
       }
-      NSGraphicsContext.restoreGraphicsState ()
+      if width > 0 { // closing the last existing rect
+        let r = QRCodeRectangle (x: originX, y: rectOriginY, width: width, height: 1)
+        rects.append (r)
+      }
     }
   //--- Add Frame
     if inFramed {
