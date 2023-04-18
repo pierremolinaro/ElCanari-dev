9674713b3e77f8d436796a104abf01c3b5bd36b2
pierremolinaro <pierre@pcmolinaro.name>
2023-04-18 20:26:48 +0200

    Résolution redimensionnement des images

--- ElCanari/auto-layout/AutoLayoutDroppableImageView.swift
+++ ElCanari/auto-layout/AutoLayoutDroppableImageView.swift
@@ -9,12 +9,143 @@
 import AppKit
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// https://stackoverflow.com/questions/44537356/swift-4-nsfilenamespboardtype-not-available-what-to-use-instead-for-registerfo
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-final class AutoLayoutDroppableImageView : NSImageView {
+final class AutoLayoutDroppableImageView : AutoLayoutVerticalStackView {
 
   //····················································································································
 
-  private var mDroppedImageCallBack : Optional < (Data) -> Void> = nil
+  private let mImageView = AutoLayoutInternalDroppableImageView ()
+  private let mRemoveButton = AutoLayoutButton (title: "-", size: .small)
+  private let mPasteImageButton = AutoLayoutButton (title: "Paste Image", size: .small)
+  private let mCopyImageButton = AutoLayoutButton (title: "Copy Image", size: .small)
+
+  //····················································································································
+
+  override init () {
+    super.init ()
+
+    self.mImageView.mDroppableImageView = self
+
+    _ = self.appendView (self.mImageView)
+    _ = self.mRemoveButton.bind_run (target: self, selector: #selector (Self.removeImageAction (_:)))
+    _ = self.mPasteImageButton.bind_run (target: self, selector: #selector (Self.pasteImageAction (_:)))
+    _ = self.mCopyImageButton.bind_run (target: self, selector: #selector (Self.copyImageAction (_:)))
+    let hStack = AutoLayoutHorizontalStackView ()
+      .appendView (self.mRemoveButton)
+      .appendView (self.mPasteImageButton)
+      .appendView (self.mCopyImageButton)
+      .appendFlexibleSpace ()
+    _ = self.appendView (hStack)
+  }
+
+ //····················································································································
+
+  required init? (coder inCoder : NSCoder) {
+    fatalError ("init(coder:) has not been implemented")
+  }
+
+  //····················································································································
+
+  func set (maxWidth inMaxWidth : Int) -> Self {
+    self.mImageView.set (maxWidth: inMaxWidth)
+    return self
+  }
+
+  //····················································································································
+
+  func set (maxHeight inMaxHeight : Int) -> Self {
+    self.mImageView.set (maxHeight: inMaxHeight)
+    return self
+  }
+
+  //····················································································································
+
+  @objc func removeImageAction (_ inUnusedSender : Any?) {
+    self.setModel (Data ())
+  }
+
+  //····················································································································
+
+  @objc func copyImageAction (_ inUnusedSender : Any?) {
+    if let data = self.mModel?.optionalValue {
+      let pasteboard = NSPasteboard.general
+      pasteboard.clearContents ()
+      pasteboard.setData (data, forType: .tiff)
+    }
+  }
+
+  //····················································································································
+
+  @objc func pasteImageAction (_ inUnusedSender : Any?) {
+    if let pdfData = NSPasteboard.general.data (forType: .pdf) {
+      self.setModel (pdfData)
+    }else if let pngData = NSPasteboard.general.data (forType: .png) {
+      self.setModel (pngData)
+    }else if let tiffData = NSPasteboard.general.data (forType: .tiff) {
+      self.setModel (tiffData)
+    }else if let tiffData = NSImage (pasteboard: NSPasteboard.general)?.tiffRepresentation {
+      self.setModel (tiffData)
+    }else if let window = self.window {
+      NSSound.beep ()
+      let alert = NSAlert ()
+      alert.messageText = "Cannot paste an image."
+      alert.informativeText = "The pasteboard does not contain a valid image."
+      alert.beginSheetModal (for: window)
+    }
+  }
+
+  //····················································································································
+  //  $imageData binding
+  //····················································································································
+
+  func updateImageData (_ object : EBReadOnlyProperty_Data) {
+    switch object.selection {
+    case .empty, .multiple :
+      self.mImageView.image = nil
+      self.mRemoveButton.isEnabled = false
+    case .single (let data) :
+      self.mImageView.image = NSImage (data: data)
+      self.mRemoveButton.isEnabled = !data.isEmpty
+    }
+  }
+
+  //····················································································································
+
+  private var mImageDataController : EBObservablePropertyController? = nil
+  private weak var mModel : EBReadWriteProperty_Data? = nil
+
+  //····················································································································
+
+  fileprivate func setModel (_ inData : Data) {
+    _ = self.mModel?.setProp (inData)
+  }
+
+  //····················································································································
+
+  final func bind_imageData (_ inModel : EBReadWriteProperty_Data) -> Self {
+    self.mModel = inModel
+    self.mImageDataController = EBObservablePropertyController (
+      observedObjects: [inModel],
+      callBack: { [weak self] in self?.updateImageData (inModel) }
+     )
+     return self
+  }
+
+  //····················································································································
+
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+fileprivate final class AutoLayoutInternalDroppableImageView : NSImageView {
+
+  //····················································································································
+
+  private var mMaxWidth : CGFloat? = nil
+  private var mMaxHeight : CGFloat? = nil
+  public weak var mDroppableImageView : AutoLayoutDroppableImageView? = nil
 
   //····················································································································
 
@@ -26,22 +157,24 @@ final class AutoLayoutDroppableImageView : NSImageView {
     self.imageScaling = .scaleProportionallyUpOrDown
     self.imageFrameStyle = .grayBezel
 
-//    self.setContentHuggingPriority (.defaultHigh, for: .vertical)
-
     self.registerForDraggedTypes (myPasteboardImageTypes ())
   }
 
  //····················································································································
 
-// override var image : NSImage? {
-//   set { super.image = newValue ; self.needsLayout = true }
-//   get { super.image }
-// }
-
- //····················································································································
-
  override var intrinsicContentSize : NSSize {
-   return self.image?.size ?? NSSize (width: 10, height: 10)
+   if let imageSize = self.image?.size {
+     var s = imageSize
+     if let maxWidth = self.mMaxWidth, s.width > maxWidth {
+       s = NSSize (width: maxWidth, height: s.height * maxWidth / s.width)
+     }
+     if let maxHeight = self.mMaxHeight, s.height > maxHeight {
+       s = NSSize (width: s.width * maxHeight / s.height, height: maxHeight)
+     }
+     return s
+   }else{
+     return super.intrinsicContentSize
+   }
  }
 
  //····················································································································
@@ -58,15 +191,16 @@ final class AutoLayoutDroppableImageView : NSImageView {
 
   //····················································································································
 
-//  override var intrinsicContentSize : NSSize {
-//    let s = super.intrinsicContentSize
-//    return NSSize (width: self.mImageWidth, height: s.height)
-//  }
+  func set (maxWidth inMaxWidth : Int) {
+    self.mMaxWidth = CGFloat (inMaxWidth)
+    self.invalidateIntrinsicContentSize ()
+  }
 
   //····················································································································
 
-  func set (droppedImageCallBack inCallBack : @escaping (Data) -> Void) {
-    self.mDroppedImageCallBack = inCallBack
+  func set (maxHeight inMaxHeight : Int) {
+    self.mMaxHeight = CGFloat (inMaxHeight)
+    self.invalidateIntrinsicContentSize ()
   }
 
   //····················································································································
@@ -89,7 +223,7 @@ final class AutoLayoutDroppableImageView : NSImageView {
   //····················································································································
 
   override func draggingEntered (_ inSender : NSDraggingInfo) -> NSDragOperation {
-    var accepts = self.mDroppedImageCallBack != nil
+    var accepts = self.mDroppableImageView != nil
     if accepts {
       let draggingPasteboard = inSender.draggingPasteboard
       accepts = NSImage.canInit (with: draggingPasteboard)
@@ -125,13 +259,13 @@ final class AutoLayoutDroppableImageView : NSImageView {
   override func concludeDragOperation (_ inSender : NSDraggingInfo?) {
     if let pboard = inSender?.draggingPasteboard {
       if let pdfData = pboard.data (forType: .pdf) {
-        self.mDroppedImageCallBack? (pdfData)
+        self.mDroppableImageView?.setModel (pdfData)
       }else if let pngData = pboard.data (forType: .png) {
-        self.mDroppedImageCallBack? (pngData)
+        self.mDroppableImageView?.setModel (pngData)
       }else if let tiffData = pboard.data (forType: .tiff) {
-        self.mDroppedImageCallBack? (tiffData)
+        self.mDroppableImageView?.setModel (tiffData)
       }else if let tiffData = NSImage (pasteboard: pboard)?.tiffRepresentation {
-        self.mDroppedImageCallBack? (tiffData)
+        self.mDroppableImageView?.setModel (tiffData)
       }
     }
   }
