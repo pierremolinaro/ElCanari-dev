03602972654012c84f77ee7f3b474040651b0506
pierremolinaro <pierre@pcmolinaro.name>
2023-09-10 20:49:14 +0200

    Update extension-AutoLayoutProjectDocument-launchFreeRouterWithRouterDocument.swift

--- ElCanari/document-project-AutoLayoutProjectDocument/extension-AutoLayoutProjectDocument-launchFreeRouterWithRouterDocument.swift
+++ ElCanari/document-project-AutoLayoutProjectDocument/extension-AutoLayoutProjectDocument-launchFreeRouterWithRouterDocument.swift
@@ -15,58 +15,64 @@ extension AutoLayoutProjectDocument {
   //····················································································································
 
   func performLaunchFreeRouterWithRouterDocument () {
-    let fm = FileManager ()
-  //---------- Get freerouter temporary directory
-    let freerouterTemporaryBaseFilePath : String
-    if let d = self.mFreerouterTemporaryDocumentDirectory {
-      freerouterTemporaryBaseFilePath = d
-    }else{
-      let h = UInt (bitPattern: Date ().hashValue)
-      freerouterTemporaryBaseFilePath = NSTemporaryDirectory () + "\(h)/"
-      do {
-        try fm.createDirectory (at: URL (fileURLWithPath: freerouterTemporaryBaseFilePath), withIntermediateDirectories: false, attributes: nil)
-      }catch (_) {
-        let alert = NSAlert ()
-        alert.messageText = "Cannot launch FreeRouting application"
-        alert.informativeText = "Cannot create \"\(freerouterTemporaryBaseFilePath)\" directory"
-        alert.beginSheetModal (for: self.windowForSheet!) { (NSModalResponse) in }
-        return
+    if let mainWindow = self.windowForSheet {
+      let fm = FileManager ()
+    //---------- Get freerouter temporary directory
+      let freerouterTemporaryBaseFilePath : String
+      if let d = self.mFreerouterTemporaryDocumentDirectory {
+        freerouterTemporaryBaseFilePath = d
+      }else{
+        let h = UInt (bitPattern: Date ().hashValue)
+        freerouterTemporaryBaseFilePath = NSTemporaryDirectory () + "\(h)/"
+        do {
+          try fm.createDirectory (at: URL (fileURLWithPath: freerouterTemporaryBaseFilePath), withIntermediateDirectories: false, attributes: nil)
+        }catch (_) {
+          let alert = NSAlert ()
+          alert.messageText = "Cannot launch FreeRouting application"
+          alert.informativeText = "Cannot create \"\(freerouterTemporaryBaseFilePath)\" directory"
+          alert.beginSheetModal (for: mainWindow) { (NSModalResponse) in }
+          return
+        }
+        self.mFreerouterTemporaryDocumentDirectory = freerouterTemporaryBaseFilePath
       }
-      self.mFreerouterTemporaryDocumentDirectory = freerouterTemporaryBaseFilePath
-    }
-    // Swift.print ("freerouterTemporaryBaseFilePath \(freerouterTemporaryBaseFilePath)")
-  //---------- Write gui_default.par
-    if preferences_mFreeRouterGuiDefaultFileContents_property.propval != "" {
-      let guiDefaultPath = freerouterTemporaryBaseFilePath + "gui_defaults.par"
-      try? preferences_mFreeRouterGuiDefaultFileContents_property.propval.write (to: URL (fileURLWithPath: guiDefaultPath), atomically: true, encoding: .utf8)
-      // Swift.print ("WRITE PATH \(guiDefaultPath)")
-    }
-  //---------- Build freerouter document
-    let exportTracks : Bool = self.rootObject.mExportExistingTracksAndVias
-    let s = self.dsnContents (exportTracks)
-  //---------- Write DSN file
-    let dsnFilePath = freerouterTemporaryBaseFilePath + "design.dsn"
-    // Swift.print ("freerouterTemporaryBaseFilePath \(freerouterTemporaryBaseFilePath)")
-    do{
-      try s.write (to: URL (fileURLWithPath: dsnFilePath), atomically: true, encoding: .utf8)
-    //--- Launch free router with document
-      if let freeRouterApplication : URL = self.uncompressedFreeRouterURL () {
-        let openConfiguration = NSWorkspace.OpenConfiguration ()
-        openConfiguration.arguments = ["-de", dsnFilePath, "-oit", "0.0"]
-        NSWorkspace.shared.openApplication (at: freeRouterApplication, configuration: openConfiguration) { (optionalApplication, optionalError) in
-          if optionalApplication == nil {
-            let alert = NSAlert ()
-            alert.messageText = "Cannot launch FreeRouting application"
-            alert.informativeText = "FreeRouting application does not exist."
-            alert.beginSheetModal (for: self.windowForSheet!) { (NSModalResponse) in }
+      // Swift.print ("freerouterTemporaryBaseFilePath \(freerouterTemporaryBaseFilePath)")
+    //---------- Write gui_default.par
+      if preferences_mFreeRouterGuiDefaultFileContents_property.propval != "" {
+        let guiDefaultPath = freerouterTemporaryBaseFilePath + "gui_defaults.par"
+        try? preferences_mFreeRouterGuiDefaultFileContents_property.propval.write (to: URL (fileURLWithPath: guiDefaultPath), atomically: true, encoding: .utf8)
+        // Swift.print ("WRITE PATH \(guiDefaultPath)")
+      }
+    //---------- Build freerouter document
+      let exportTracks : Bool = self.rootObject.mExportExistingTracksAndVias
+      let s = self.dsnContents (exportTracks)
+    //---------- Write DSN file
+      let dsnFilePath = freerouterTemporaryBaseFilePath + "design.dsn"
+      // Swift.print ("freerouterTemporaryBaseFilePath \(freerouterTemporaryBaseFilePath)")
+      do{
+        try s.write (to: URL (fileURLWithPath: dsnFilePath), atomically: true, encoding: .utf8)
+      //--- Launch free router with document
+        if let freeRouterApplication : URL = self.uncompressedFreeRouterURL () {
+          let openConfiguration = NSWorkspace.OpenConfiguration ()
+          openConfiguration.arguments = ["-de", dsnFilePath, "-oit", "0.0"]
+          NSWorkspace.shared.openApplication (at: freeRouterApplication, configuration: openConfiguration) { (optionalApplication, optionalError) in
+            if optionalApplication == nil {
+              DispatchQueue.main.async {
+                let alert = NSAlert ()
+                alert.messageText = "Cannot launch FreeRouting application"
+                alert.informativeText = "FreeRouting application does not exist."
+                alert.beginSheetModal (for: mainWindow) { (NSModalResponse) in }
+              }
+            }
           }
         }
+      }catch (_) {
+        DispatchQueue.main.async {
+          let alert = NSAlert ()
+          alert.messageText = "Cannot launch FreeRouting application"
+          alert.informativeText = "Cannot write \(dsnFilePath) file"
+          alert.beginSheetModal (for: mainWindow) { (NSModalResponse) in }
+        }
       }
-    }catch (_) {
-      let alert = NSAlert ()
-      alert.messageText = "Cannot launch FreeRouting application"
-      alert.informativeText = "Cannot write \(dsnFilePath) file"
-      alert.beginSheetModal (for: self.windowForSheet!) { (NSModalResponse) in }
     }
   }
 
