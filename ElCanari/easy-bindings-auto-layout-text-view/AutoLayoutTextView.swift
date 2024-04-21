f6487738fd288528c0b00dedc73b9e1d75dcc05b
pierremolinaro <pierre@pcmolinaro.name>
2024-04-21 22:58:46 +0200

    Update AutoLayoutTextView.swift

--- ElCanari/easy-bindings-auto-layout-text-view/AutoLayoutTextView.swift
+++ ElCanari/easy-bindings-auto-layout-text-view/AutoLayoutTextView.swift
@@ -14,7 +14,6 @@ final class AutoLayoutTextView : NSScrollView {
 
   init () {
     super.init (frame: .zero)
-//    super.init (frame: NSRect (x: 0, y: 0, width: 100, height: 100))
     noteObjectAllocation (self)
     self.translatesAutoresizingMaskIntoConstraints = false
 
