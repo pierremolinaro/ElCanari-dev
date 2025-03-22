2de16ba6d1b20bcf54d825aaa1baf27e9a67c3c1
pierremolinaro <pierre@pcmolinaro.name>
2025-03-22 12:54:58 +0100

    Correction affichage édition net class

--- ElCanari/document-project-extensions/extension-AutoLayoutProjectDocument-add-edit-net-class.swift
+++ ElCanari/document-project-extensions/extension-AutoLayoutProjectDocument-add-edit-net-class.swift
@@ -19,7 +19,7 @@ extension AutoLayoutProjectDocument {
                                          callBack inCallBack : @escaping () -> Void) {
     if let window = self.windowForSheet {
       let panel = NSPanel (
-        contentRect: NSRect (x: 0, y: 0, width: 500, height: 500),
+        contentRect: NSRect (x: 0, y: 0, width: 400, height: 500),
         styleMask: [.titled],
         backing: .buffered,
         defer: false
@@ -35,7 +35,7 @@ extension AutoLayoutProjectDocument {
       let netClassNameTextField = AutoLayoutTextField (minWidth: 100, size: .regular).expandableWidth ()
       netClassNameTextField.stringValue = inNetClass.mNetClassName
       do{
-        let left = AutoLayoutStaticLabel (title: "Net Class", bold: false, size: .regular, alignment: .right)
+        let left = AutoLayoutStaticLabel (title: "Net Class Name", bold: false, size: .regular, alignment: .right)
         _ = gridView.append (left: left, right: netClassNameTextField)
       }
     //---
@@ -57,30 +57,6 @@ extension AutoLayoutProjectDocument {
         let left = AutoLayoutStaticLabel (title: "Track Width", bold: false, size: .regular, alignment: .right)
         _ = gridView.append (left: left, right: widthFields)
       }
-    //---  Allow front track
-      let allowFrontTrack_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnFrontSide, undoManager: nil, key: nil)
-      let allowFrontTrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Front Layer", size: .regular).bind_value (allowFrontTrack_property).expandableWidth ()
-      _ = gridView.append (left: AutoLayoutHorizontalStackView (), right: allowFrontTrackCheckBox)
-    //---  Allow Inner 1 Layer
-      let allowInner1Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner1Layer, undoManager: nil, key: nil)
-      let allowInner1TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 1 Layer", size: .regular).bind_value (allowInner1Layer_property).expandableWidth ()
-      _ = gridView.append (left: AutoLayoutHorizontalStackView (), right: allowInner1TrackCheckBox)
-    //---  Allow Inner 2 Layer
-      let allowInner2Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner2Layer, undoManager: nil, key: nil)
-      let allowInner2TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 2 Layer", size: .regular).bind_value (allowInner2Layer_property).expandableWidth ()
-      _ = gridView.append (left: AutoLayoutHorizontalStackView (), right: allowInner2TrackCheckBox)
-    //---  Allow Inner 3 Layer
-      let allowInner3Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner3Layer, undoManager: nil, key: nil)
-      let allowInner3TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 3 Layer", size: .regular).bind_value (allowInner3Layer_property).expandableWidth ()
-      _ = gridView.append (left: AutoLayoutHorizontalStackView (), right: allowInner3TrackCheckBox)
-    //---  Allow Inner 4 Layer
-      let allowInner4Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner4Layer, undoManager: nil, key: nil)
-      let allowInner4TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 4 Layer", size: .regular).bind_value (allowInner4Layer_property).expandableWidth ()
-      _ = gridView.append (left: AutoLayoutHorizontalStackView (), right: allowInner4TrackCheckBox)
-    //---  Allow Back track
-      let allowBackTrack_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnBackSide, undoManager: nil, key: nil)
-      let allowBackTrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Back Layer", size: .regular).bind_value (allowBackTrack_property).expandableWidth ()
-      _ = gridView.append (left: AutoLayoutHorizontalStackView (), right: allowBackTrackCheckBox)
     //--- Hole Diameter
       let viaHoleDiameter_property = EBStoredProperty_Int (defaultValue: inNetClass.mViaHoleDiameter, undoManager: nil, key: nil)
       let viaHoleDiameterUnit_property = EBStoredProperty_Int (defaultValue: inNetClass.mViaHoleDiameterUnit, undoManager: nil, key: nil)
@@ -97,16 +73,40 @@ extension AutoLayoutProjectDocument {
         let left = AutoLayoutStaticLabel (title: "Via Pad Diameter", bold: false, size: .regular, alignment: .right)
         _ = gridView.append (left: left, right: padDiameterFields)
       }
-    //---
       _ = layoutView.appendView (gridView)
+    //---  Allow front track
+      let allowFrontTrack_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnFrontSide, undoManager: nil, key: nil)
+      let allowFrontTrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Front Layer", size: .regular).bind_value (allowFrontTrack_property).expandableWidth ()
+      _ = layoutView.appendView (allowFrontTrackCheckBox)
+    //---  Allow Inner 1 Layer
+      let allowInner1Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner1Layer, undoManager: nil, key: nil)
+      let allowInner1TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 1 Layer", size: .regular).bind_value (allowInner1Layer_property).expandableWidth ()
+      _ = layoutView.appendView (allowInner1TrackCheckBox)
+    //---  Allow Inner 2 Layer
+      let allowInner2Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner2Layer, undoManager: nil, key: nil)
+      let allowInner2TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 2 Layer", size: .regular).bind_value (allowInner2Layer_property).expandableWidth ()
+      _ = layoutView.appendView (allowInner2TrackCheckBox)
+    //---  Allow Inner 3 Layer
+      let allowInner3Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner3Layer, undoManager: nil, key: nil)
+      let allowInner3TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 3 Layer", size: .regular).bind_value (allowInner3Layer_property).expandableWidth ()
+      _ = layoutView.appendView (allowInner3TrackCheckBox)
+    //---  Allow Inner 4 Layer
+      let allowInner4Layer_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnInner4Layer, undoManager: nil, key: nil)
+      let allowInner4TrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Inner 4 Layer", size: .regular).bind_value (allowInner4Layer_property).expandableWidth ()
+      _ = layoutView.appendView (allowInner4TrackCheckBox)
+    //---  Allow Back track
+      let allowBackTrack_property = EBStoredProperty_Bool (defaultValue: inNetClass.mAllowTracksOnBackSide, undoManager: nil, key: nil)
+      let allowBackTrackCheckBox = AutoLayoutCheckbox (title: "Allow Tracks on Back Layer", size: .regular).bind_value (allowBackTrack_property).expandableWidth ()
+      _ = layoutView.appendView (allowBackTrackCheckBox)
+    //---
       _ = layoutView.appendFlexibleSpace ()
       let okButtonTitle = inCreation ? "Add New Net" : "Commit Changes"
       let okButton = AutoLayoutSheetDefaultOkButton (title: okButtonTitle, size: .regular, sheet: panel)
       do{
         let hStack = AutoLayoutHorizontalStackView ()
-        _ = hStack.appendView (AutoLayoutSheetCancelButton (title: "Cancel", size: .regular))
-        _ = hStack.appendFlexibleSpace ()
-        _ = hStack.appendView (okButton)
+          .appendView (AutoLayoutSheetCancelButton (title: "Cancel", size: .regular))
+          .appendFlexibleSpace ()
+          .appendView (okButton)
         _ = layoutView.appendView (hStack)
       }
     //---
